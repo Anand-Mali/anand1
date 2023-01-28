1378dfbbecc8ff792dc496308a58ff0b895cfa4e
anand <anandmali1890@gmail.com>
2023-01-28 10:43:52 +0530

    tis is my second commit

--- src/demo/first.java
+++ src/demo/first.java
@@ -1,4 +1,5 @@
 package demo;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,11 +7,14 @@ class first {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\seljar\\chromedriver_win32\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\seljar\\chromedriver\\chromedriver.exe");
          WebDriver driver=new ChromeDriver();
          driver.get("http://www.google.com");
-//         Thread.sleep (2000);
-//         driver.manage().window().maximize();
+         Thread.sleep (2000);
+         driver.manage().window().maximize();
+         driver.findElement(By.xpath("//input[@name=\"q\"]")).sendKeys("Anand mali");
+         Thread.sleep(3000);
+         driver.close();
 	}
 
 }
